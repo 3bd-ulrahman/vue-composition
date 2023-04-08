af4b623ef5593040299f343f215594ea77bfb4d3
3bd-ulrahman <abdulrahman198276301@gmail.com>
2023-04-08 17:56:18 +0200

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="home">
+
+    <h2>{{ appTitle }}</h2>
+
+    <h3>{{ counterData.title }}:</h3>
+
+    <div>
+      <button @click="decreaseCounter(2)" class="btn">--</button>
+      <button @click="decreaseCounter(1)" class="btn">-</button>
+      <span class="counter">{{ counterData.count }}</span>
+      <button @click="increaseCounter(1)" class="btn">+</button>
+      <button @click="increaseCounter(2)" class="btn">++</button>
+    </div>
+
+    <p>This counter is {{ oddOrEven }}</p>
+
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input v-model="counterData.title" type="text">
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+import { reactive, computed, watch } from 'vue';
+
+const appTitle = 'My Amazing Conter App';
+
+const counterData = reactive({
+  count: 0,
+  title: 'My Counter'
+});
+
+watch(() => counterData.count, (newCount, oldCount) => {
+  if (newCount === 20) {
+    alert('Way to go! you made it 20!!')
+  }
+});
+
+const oddOrEven = computed(() => {
+  if (counterData.count % 2 === 0) return 'even';
+
+  return 'odd';
+});
+
+const increaseCounter = (amount) => {
+  counterData.count += amount;
+}
+
+const decreaseCounter = (amount) => {
+  counterData.count -= amount;
+}
+</script>
+
+<!-- <script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const counter = ref(0);
+
+    const increaseCounter = () => {
+      counter.value++;
+    }
+
+    const decreaseCounter = () => {
+      counter.value--;
+    }
+
+    return {
+      counter,
+      increaseCounter,
+      decreaseCounter
+    };
+  }
+}
+</script> -->
+
+<!-- <script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increaseCounter() {
+      this.counter++;
+    },
+    decreaseCounter() {
+      this.counter--;
+    }
+  }
+}
+</script> -->
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+
+.btn,
+.counter {
+  font-size: 40px;
+  margin: 10px;
+}
+
+.edit {
+  margin-top: 60px;
+}
+</style>
